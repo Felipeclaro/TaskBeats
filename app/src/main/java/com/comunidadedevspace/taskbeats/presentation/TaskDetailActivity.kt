cf18a866e231a46bd871cdc1a961cb3e515a7d04
Felipeclaro <felipeclarouk@gmail.com>
2023-05-01 13:03:19 +0200

    Primeira Parte Android ONE Finalizada

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskDetailActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.comunidadedevspace.taskbeats.presentation
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +10,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.viewModels
 import com.comunidadedevspace.taskbeats.R
 import com.comunidadedevspace.taskbeats.data.Task
 import com.google.android.material.snackbar.Snackbar
@@ -19,11 +19,16 @@ class TaskDetailActivity : AppCompatActivity() {
 
     private var task: Task? = null
     private lateinit var btnDone: Button
-    companion object{
-        private const  val TASK_DETAIL_EXTRA = "task.extra.detail"
 
-        fun start(context: Context, task: Task?): Intent{
-            val intent = Intent (context, TaskDetailActivity::class.java)
+    private val viewModel: TaskDetailViewModel by viewModels{
+        TaskDetailViewModel.getVMFactory(application)
+    }
+
+    companion object {
+        private const val TASK_DETAIL_EXTRA = "task.extra.detail"
+
+        fun start(context: Context, task: Task?): Intent {
+            val intent = Intent(context, TaskDetailActivity::class.java)
                 .apply {
                     putExtra(TASK_DETAIL_EXTRA, task)
                 }
@@ -31,6 +36,7 @@ class TaskDetailActivity : AppCompatActivity() {
             return intent
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
@@ -43,31 +49,26 @@ class TaskDetailActivity : AppCompatActivity() {
         val edtDescription = findViewById<EditText>(R.id.edt_task_description)
         btnDone = findViewById<Button>(R.id.btn_done)
 
-        if(task != null){
+        if (task != null) {
             edtTitle.setText(task!!.title)
             edtDescription.setText(task!!.description)
         }
 
-        btnDone.setOnClickListener{
+        btnDone.setOnClickListener {
             val title = edtTitle.text.toString()
             val desc = edtDescription.text.toString()
 
-            if(title.isNotEmpty() && desc.isNotEmpty()){
-                if (task == null){
+            if (title.isNotEmpty() && desc.isNotEmpty()) {
+                if (task == null) {
                     addOrUpdateTask(0, title, desc, ActionType.CREATE)
-                }else{
+                } else {
                     addOrUpdateTask(task!!.id, title, desc, ActionType.UPDATE)
                 }
-            }else{
-                showMessage(it, "Field are required" )
+            } else {
+                showMessage(it, "Field are required")
             }
         }
 
-        // Recuperar campo do XML
-        //tvTitle = findViewById(R.id.tv_task_title_detail)
-
-        //Setar novo texto na tela
-        //tvTitle.text = task?.title ?: "Adicione uma tarefa"
     }
 
     private fun addOrUpdateTask(
@@ -75,13 +76,13 @@ class TaskDetailActivity : AppCompatActivity() {
         title: String,
         description: String,
         actionType: ActionType
-    ){
+    ) {
         val task = Task(id, title, description)
-        returnAction(task, actionType)
+        performAction(task, actionType)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater : MenuInflater = menuInflater
+        val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_task_detail, menu)
         return true
     }
@@ -90,7 +91,7 @@ class TaskDetailActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.delete_task -> {
                 if (task != null) {
-                   returnAction(task!!, ActionType.DELETE)
+                    performAction(task!!, ActionType.DELETE)
                 } else {
                     showMessage(btnDone, "Item Not Found")
                 }
@@ -101,19 +102,16 @@ class TaskDetailActivity : AppCompatActivity() {
         }
     }
 
-            private fun returnAction(task: Task, actionType: ActionType){
-                val intent = Intent()
-                    .apply {
-                        val taskAction = TaskAction(task, actionType.name)
-                        putExtra(TASK_ACTION_RESULT, taskAction)
-                    }
-                setResult(Activity.RESULT_OK, intent)
-                finish()
-            }
-            private fun showMessage (view: View, message:String){
-                Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .show()
-            }
-        }
+    private fun performAction(task: Task, actionType: ActionType) {
+        val taskAction = TaskAction(task, actionType.name)
+        viewModel.execute(taskAction)
+        finish()
+    }
+
+    private fun showMessage(view: View, message: String) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+            .setAction("Action", null)
+            .show()
+    }
+}
 
---
+++ app/src/test/java/com/comunidadedevspace/taskbeats/TaskListViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.comunidadedevspace.taskbeats
+
+import com.comunidadedevspace.taskbeats.data.Task
+import com.comunidadedevspace.taskbeats.data.TaskDao
+import com.comunidadedevspace.taskbeats.presentation.ActionType
+import com.comunidadedevspace.taskbeats.presentation.TaskAction
+import com.comunidadedevspace.taskbeats.presentation.TaskListViewModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+class TaskListViewModelTest {
+
+    private val taskDao: TaskDao = mock()
+
+    private val underTest: TaskListViewModel by lazy {
+        TaskListViewModel(taskDao)
+    }
+}
